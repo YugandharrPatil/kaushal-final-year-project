d87a7bcff457898ea7fdf68cd1a1d567cd53d7f6
yugz <yugandhar.patil@learner.manipal.edu>
2024-03-27 22:39:23 +0530

    first comomit

---
+++ src/app/components/heading.tsx
@@ -0,0 +1,3 @@
+export default function Heading({ children }: { children: React.ReactNode }) {
+	return <div className="text-2xl font-bold">{children}</div>;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,137 @@
-import Image from "next/image";
+import { config } from "dotenv";
+import Moralis from "moralis";
+import Block from "./components/block";
+import Heading from "./components/heading";
+config();
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+const getData = async () => {
+	try {
+		// response not required here, just execution
+		await Moralis.start({
+			apiKey: process.env.MORALIS_API_KEY,
+		});
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+		// CALLS
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+		// GET BLOCKS
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+		// getBlockByHash
+		const blockByHash = await Moralis.EvmApi.block.getBlock({
+			chain: "0x1",
+			blockNumberOrHash: "18541416",
+		});
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+		// getBlockByDate
+		// const blockByDate = await Moralis.EvmApi.block.getDateToBlock({
+		// 	date: "", //TODO: MISSING!!!
+		// 	chain: "0x1",
+		// });
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+		// Get Transactions
+
+		// getDecodedWalletTransaction
+		const decodedWalletTransaction = await Moralis.EvmApi.transaction.getWalletTransactionsVerbose({
+			chain: "0x1",
+			order: "DESC",
+			address: "0x1f9090aaE28b8a3dCeaDf281B0F12828e676c326",
+		});
+
+		// getTransactionByHash
+		const transactionByHash = await Moralis.EvmApi.transaction.getTransaction({
+			chain: "0x1",
+			transactionHash: "0xdc85cb1b75fd09c2f6d001fea4aba83764193cbd7881a1fa8ccde350a5681109",
+		});
+
+		// getDecodedTransactionByHash
+		const decodedTransactionByHash = await Moralis.EvmApi.transaction.getTransactionVerbose({
+			chain: "0x1",
+			transactionHash: "0x012b9b98e21664117ec0b499d726a39f492ac8bd402cca8bebcbd163b9f75760",
+		});
+
+		// getInternalTransactions
+		const internalTransactions = await Moralis.EvmApi.transaction.getInternalTransactions({
+			chain: "0x1",
+			transactionHash: "0xdc85cb1b75fd09c2f6d001fea4aba83764193cbd7881a1fa8ccde350a5681109",
+		});
+
+		// getWalletTransactions
+		const walletTransactions = await Moralis.EvmApi.transaction.getWalletTransactions({
+			chain: "0x1",
+			order: "DESC",
+			address: "0x1f9090aaE28b8a3dCeaDf281B0F12828e676c326",
+		});
+
+		// GET LOGS
+		// getContractLogs
+		// const contractLogs = await Moralis.EvmApi.events.getContractLogs({
+		// 	chain: "0x1",
+		// 	order: "DESC",
+		// 	address: "0xb47e3cd837dDF8e4c57F05d70Ab865de6e193BBB",
+		// 	topic0: "", // TODO: MISSING!!!
+		// });
+
+		// getContractEvents
+		// const contractEvents = await Moralis.EvmApi.events.getContractEvents({
+		// 	chain: "0x1",
+		// 	order: "DESC",
+		// 	address: "0xb47e3cd837dDF8e4c57F05d70Ab865de6e193BBB",
+		// 	abi: {}, // TODO: MISSING
+		// 	topic: "",
+		// });
+
+		// GET STATS
+
+		// getBlockStats
+		// const blockStats = await Moralis.EvmApi.block.getBlockStats({
+		// 	chain: "0x1",
+		// 	blockNumberOrHash: "1023323", // TODO: parse number as string dynamically
+		// });
+
+		return {
+			blocks: { blockByHash },
+			// transactions: { decodedWalletTransaction, transactionByHash, decodedTransactionByHash, internalTransactions, walletTransactions },
+			// logs: { contractLogs, contractEvents },
+			// stats: { blockStats },
+		};
+	} catch (err) {
+		console.error(err);
+	}
+};
+
+export default async function HomePage() {
+	const data = await getData();
+	const t = data?.blocks.blockByHash.raw.transactions;
+	// const logs = data?.blocks.blockByHash.raw.transactions[0].logs
+	// const internetTransactions = data?.blocks.blockByHash.raw.transactions[0].internal_transactions
+	return (
+		<main className="flex">
+			<Block>
+				<Heading>Block Stats</Heading>
+				<div>
+					{/* <p>{data?.blocks.blockByHash?.raw.timestamp}</p>
+					<p>{data?.blocks.blockByHash?.raw.number}</p>
+					<p>{data?.blocks.blockByHash?.raw.hash}</p>
+					<p>{data?.blocks.blockByHash?.raw.parent_hash}</p>
+					<p>{data?.blocks.blockByHash?.raw.nonce}</p>
+					<p>{data?.blocks.blockByHash?.raw.sha3_uncles}</p>
+					<p>{data?.blocks.blockByHash?.raw.transactions_root}</p>
+					<p>{data?.blocks.blockByHash?.raw.state_root}</p>
+					<p>{data?.blocks.blockByHash?.raw.receipts_root}</p>
+					<p>{data?.blocks.blockByHash?.raw.miner}</p>
+					<p>{data?.blocks.blockByHash?.raw.difficulty}</p>
+					<p>{data?.blocks.blockByHash?.raw.total_difficulty}</p>
+					<p>{data?.blocks.blockByHash?.raw.size}</p>
+					<p>{data?.blocks.blockByHash?.raw.extra_data}</p>
+					<p>{data?.blocks.blockByHash?.raw.gas_limit}</p>
+					<p>{data?.blocks.blockByHash?.raw.gas_used}</p>
+					<p>{data?.blocks.blockByHash?.raw.transaction_count}</p> */}
+					<h3 className="text-xl font-bold">Transactions</h3>
+					{t?.map((item) => (
+						<p key={item}>{item.block_hash}</p>
+					))}
+				</div>
+				<div className=""></div>
+			</Block>
+		</main>
+	);
 }
